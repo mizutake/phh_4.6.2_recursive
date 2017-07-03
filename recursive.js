4a9da338179f2f27c35af62f738860cb1265d6bc
mizutake <takeshi_17n1100069@.nnn.ed.jp>
2017-07-03 14:09:33 +0900

    教科書の内容の写経

--- recursive.js
+++ recursive.js
@@ -1,6 +1,26 @@
 function factorial(n) {
-    if (n != 0) { return n * factorial(n-1); }
+    if (n != 0) { return n * factorial(n - 1); }
     return 1;
 }
 
-console.log(factorial(5));
+console.log(factorial(7));
+
+function factorial(n) {
+    let x = n;
+    for (let i = 1; i < x; i++) {
+        n = n * (x - i);
+        //console.log(i);
+        //console.log(n);
+    }
+    console.log(n);
+}
+factorial(6);
+
+function factorial(n) {
+    let answer = 1;
+    for(let i = 0; i < n; i++) {
+        answer *= (n - i);
+    }
+    return answer;
+}
+console.log (factorial (5));
